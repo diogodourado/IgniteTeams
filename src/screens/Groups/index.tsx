5c9602b7375452cd08c24aa85f2b74aefa485870
Diogo Dourado <diogo@dourado.net>
2024-07-22 11:50:00 -0300

    Inicio do projeto de estudos

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,15 @@
+import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
+import { GroupCard } from '@components/GroupCard';
+
+import { Container } from './styles';
+
+export function Groups() {
+    return (
+        <Container>
+            <Header showBackButton />
+            <Highlight title="Turmas" subtitle="Jogue com a sua turma" />
+            <GroupCard title="Turma do Zezinho" />
+        </Container>
+    );
+}
---
+++ src/screens/Groups/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+        padding-top: 50px;
+        padding-right: 20px;
+        flex: 1;
+        background-color: ${({ theme }) => theme.COLORS.GRAY_500} ;
+`

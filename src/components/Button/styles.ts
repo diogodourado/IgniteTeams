fbfc74f74dc8eaed5a74f97f9585a58b33c3cda4
Diogo Dourado <diogo@dourado.net>
2024-07-22 17:51:48 -0300

    Refatorando styled com CSS

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,5 +1,5 @@
 import { TouchableOpacity } from 'react-native';
-import styled from 'styled-components/native';
+import styled, { css } from 'styled-components/native';
 
 export type ButtonTypeStyleProps = 'PRIMARY' | 'SECONDARY';
 
@@ -8,20 +8,21 @@ type Props = {
 }
 
 export const Container = styled(TouchableOpacity) <Props>`
-flex: 1;
-min-height: 56px;
-max-height: 56px;
-background-color: ${({ theme, type }) => type === 'PRIMARY' ? theme.COLORS.GREEN_700 : theme.COLORS.RED_DARK};
-border-radius: 6px;
-justify-content: center;
-align-items: center;
-
+        flex: 1;
+        min-height: 56px;
+        max-height: 56px;
+        background-color: ${({ theme, type }) => type === 'PRIMARY' ? theme.COLORS.GREEN_700 : theme.COLORS.RED_DARK};
+        border-radius: 6px;
+        justify-content: center;
+        align-items: center;
 `
 
 export const Title = styled.Text`
-        text-align: center;
-        font-size: ${({ theme }) => theme.FONT_SIZE.MD}px;
-        font-family: ${({ theme }) => theme.FONT_FAMILY.BOLD};
-        color: ${({ theme }) => theme.COLORS.WHITE};
+        ${({ theme }) => css`
+                text-align: center;
+                font-size: ${theme.FONT_SIZE.MD}px;
+                font-family: ${theme.FONT_FAMILY.BOLD};
+                color: ${theme.COLORS.WHITE};
+        `};
         
 `
--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,17 +1,19 @@
-import styled from "styled-components/native";
+import styled, { css } from "styled-components/native";
 import { TextInput } from "react-native";
 
 export const Container = styled(TextInput)`
+
+    ${({ theme }) => css`
+        font-size: ${theme.FONT_SIZE.MD}px;
+        font-family: ${theme.FONT_FAMILY.REGULAR};
+        background-color: ${theme.COLORS.GRAY_700};
+        color: ${theme.COLORS.WHITE};
+    `};
+
     flex: 1;
     min-height: 56px;
     max-height: 56px;
 
-    font-size: ${({ theme }) => theme.FONT_SIZE.MD}px;
-    font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
-    
-    background-color: ${({ theme }) => theme.COLORS.GRAY_700};
-    color: ${({ theme }) => theme.COLORS.WHITE};
-    
     border-radius: 6px;
     padding: 16px;
 `

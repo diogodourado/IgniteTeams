ff78a6f2aba6f258474301cf3a1d110b05427308
Diogo Dourado <diogo@dourado.net>
2024-07-22 13:24:08 -0300

    Adicionando aviso da Lista Vazia

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,6 +4,7 @@ import { FlatList } from 'react-native';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
+import { ListEmpty } from '@components/ListEmpty';
 
 import { Container } from './styles';
 
@@ -23,6 +24,8 @@ export function Groups() {
                         title={item}
                     />
                 )}
+                contentContainerStyle={groups.length === 0 && { flex: 1 }}
+                ListEmptyComponent={() => <ListEmpty message="Que tal cadastrar a primeira turma?" />}
             />
 
         </Container>

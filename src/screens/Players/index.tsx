ce36fd254ce1729a2cb828a2403f054567c851d4
Diogo Dourado <diogo@dourado.net>
2024-07-23 17:37:11 -0300

    Finalizando a lista de Players

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,19 +1,21 @@
 import { useState } from "react";
 import { FlatList } from "react-native";
 
-import { Header } from "@components/Header";
-import { Filter } from "@components/Filters";
-import { Highlight } from "@components/Highlight";
-import { ButtonIcon } from "@components/ButtonIcon";
 import { Input } from "@components/Input";
+import { Filter } from "@components/Filters";
+import { Header } from "@components/Header";
+import { Highlight } from "@components/Highlight";
+import { ListEmpty } from "@components/ListEmpty";
+import { ButtonIcon } from "@components/ButtonIcon";
+import { PlayerCard } from "@components/PlayerCard";
 
 import { Container, Form, HeaderList, NumbersOfPlayers } from "./styles";
-import { PlayerCard } from "@components/PlayerCard";
+import { Button } from "@components/Button";
 
 export function Players() {
 
     const [team, setTeam] = useState('Time A');
-    const [players, setPlayers] = useState(['Diogo', 'Dourado']);
+    const [players, setPlayers] = useState(['Diogo', 'Rafael', 'Ana', 'Yanne', 'Rodrigo', 'Cesar', 'Paulo', 'José', 'Maria']);
 
     return (
         <Container>
@@ -60,6 +62,17 @@ export function Players() {
                         onRemove={() => { }}
                     />
                 )}
+                ListEmptyComponent={() => <ListEmpty message="Não há pessoas neste time" />}
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={[
+                    { paddingBottom: 100 },
+                    players.length === 0 && { flex: 1 }
+                ]}
+            />
+
+            <Button
+                title="Remover Turma"
+                type="SECONDARY"
             />
 
         </Container>

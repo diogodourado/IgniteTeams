e57aa38bafbc2fb59530e0072cb7f438f4b6acba
Diogo Dourado <diogo@dourado.net>
2024-07-26 18:04:34 -0300

    Filtrando por time

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Alert, FlatList } from "react-native";
 import { useRoute } from "@react-navigation/native";
 
@@ -45,6 +45,7 @@ export function Players() {
 
         try {
             await playerAddByGroup(newPlayer, group);
+            fetchPlayersByTeam();
 
 
         } catch (error) {
@@ -70,6 +71,10 @@ export function Players() {
             }
         }
     }
+
+    useEffect(() => {
+        fetchPlayersByTeam();
+    }, [team])
 
     return (
         <Container>
@@ -111,10 +116,10 @@ export function Players() {
 
             <FlatList
                 data={players}
-                keyExtractor={item => item}
+                keyExtractor={item => item.name}
                 renderItem={({ item }) => (
                     <PlayerCard
-                        name={item}
+                        name={item.name}
                         onRemove={() => { }}
                     />
                 )}
